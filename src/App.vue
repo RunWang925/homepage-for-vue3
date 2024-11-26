7c893130e8dff30444c44ffc7a69530edee4c372
佩奇 <cqwangrui925@gmail.com>
2024-11-26 14:39:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@
     </div>
 
     <div class="footer">
-      By <a href="https://814925.xyz/" target="_blank">佩奇</a> | ©2024
+      <b><a href="https://814925.xyz/" target="_blank">By佩奇©2024</a></b> | ©2024  | <b><a href="https://beian.miit.gov.cn/" target="_blank">渝ICP备-2024044325号</a></b>
     </div>
 
     <vs-dialog overlay-blur width="550px" not-center v-model="active">
