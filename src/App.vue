a8079c2ea3cf1cfc19c9f5da7d152bb82ae156f3
佩奇 <cqwangrui925@gmail.com>
2025-08-22 11:05:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,18 +56,20 @@
 
     <div class="footer">
       <b>©2024By佩奇</b> | <b><a href="https://beian.miit.gov.cn/" target="_blank">渝ICP备-2024044325号</a></b>
-|<b>
-  <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50023302000356" rel="noreferrer" target="_blank">
-    <img src="https://beian.mps.gov.cn/web/resource/img/head-logo.png" 
-         alt="备案图标" 
-         style="vertical-align: middle; 
-                margin-right: 5px; 
-                height: 1em; 
-                width: auto; /* 保持宽高比 */
-                display: inline-block; /* 确保正确渲染 */">
-    渝公网安备50023302000356号
-  </a>
-</b> </div>
+| <b>
+        <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50023302000356" rel="noreferrer" target="_blank" 
+           style="text-decoration: none; color: inherit; display: inline-flex; align-items: center;">
+            <img src="https://beian.mps.gov.cn/web/resource/img/head-logo.png" 
+                 alt="公安备案图标" 
+                 style="margin-right: 5px; 
+                        height: 1em; 
+                        width: auto;
+                        min-width: 1em;
+                        min-height: 1em;
+                        transition: opacity 0.2s ease;">
+            渝公网安备50023302000356号
+        </a>
+    </b> </div>
 
     <vs-dialog overlay-blur width="550px" not-center v-model="active">
       <template #header>
