641cb5b118f83082c9a8a954c1a3497e0328c303
佩奇 <cqwangrui925@gmail.com>
2025-08-22 09:09:35 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 
     <div class="footer">
       <b>©2024By佩奇</b> | <b><a href="https://beian.miit.gov.cn/" target="_blank">渝ICP备-2024044325号</a></b>
-| <b><a href="https://beian.miit.gov.cn/" target="_blank">渝ICP备-2024044325号</a></b>    </div>
+| <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50023302000356" rel="noreferrer" target="_blank">渝公网安备50023302000356号</a>    </div>
 
     <vs-dialog overlay-blur width="550px" not-center v-model="active">
       <template #header>
